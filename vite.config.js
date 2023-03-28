e5ffe9dd87c454be2fc0296af065e454b202131c
Melchor Callos <melchorcallos04@gmail.com>
2023-03-29 00:20:48 +0800

    Fix: Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/food-recipe-website",
+  base: "/food-recipe-website/",
   plugins: [react()],
 });
