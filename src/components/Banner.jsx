4ad03ea17eef234a5f24b462ed41e2b64cc1b7f8
Melchor Callos <melchorcallos04@gmail.com>
2023-03-31 02:48:00 +0800

    Fix: Image slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,39 @@ import HeroSlider, { Overlay, Slide } from "hero-slider";
 import { useMealContext } from "../context/Context";
 import { updateMealListBySearch } from "../utilities/Action";
 import { styles } from "../styles/BannerStyles";
-import { imageList } from "../utilities/Images";
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.jpg";
+import img6 from "../assets/images/img6.jpg";
+import img7 from "../assets/images/img7.jpg";
+import img8 from "../assets/images/img8.jpg";
+import img9 from "../assets/images/img9.jpg";
+import img10 from "../assets/images/img10.jpg";
+import img11 from "../assets/images/img11.jpg";
+import img12 from "../assets/images/img12.jpg";
+import img13 from "../assets/images/img13.jpg";
+import img14 from "../assets/images/img14.jpg";
+import img15 from "../assets/images/img15.jpg";
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+];
 
 const Banner = () => {
   const {
@@ -82,13 +114,8 @@ const Banner = () => {
             </Box>
           </Box>
         </Overlay>
-        {imageList.map((image, index) => (
-          <Slide
-            key={index}
-            background={{
-              backgroundImageSrc: `${image}`,
-            }}
-          />
+        {images.map((image, index) => (
+          <Slide key={index} background={{ backgroundImageSrc: image }} />
         ))}
       </HeroSlider>
     </>
